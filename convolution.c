eac134cc8f663c5c997f307639791de8fa1036a8
Aaron Dawson <adawson927@gmail.com>
2015-09-21 17:44:33 -0400

    fft in callback function

--- convolution.c
+++ convolution.c
@@ -17,10 +17,13 @@
 #include <ctype.h>
 #include <sndfile.h>
 #include <math.h>
+#include <limits.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <portaudio.h>
 #include <ncurses.h>
+#include <sys/resource.h>
+#include <sys/times.h>
 #include "dawsonaudio.h"
 #include "convolve.h"
 #include "vector.h"
@@ -35,6 +38,18 @@ typedef struct FFTData {
 	int size;
 } FFTData;
 
+/* Returns number of seconds between b and a. */
+double calculate(struct rusage *b, struct rusage *a) {
+	if (b == NULL || a == NULL)
+		return 0;
+	else
+		return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec)
+				- (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec))
+				+ ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec)
+						- (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
+				/ 1000000.);
+}
+
 BlockData *allocateBlockBuffers(Vector vector) {
 	BlockData* data_ptr = (BlockData*) malloc(sizeof(BlockData));
 	data_ptr->audioBlocks = (float**) malloc(sizeof(float*) * vector.size);
@@ -43,7 +58,7 @@ BlockData *allocateBlockBuffers(Vector vector) {
 	for (i = 0; i < vector.size; i++) {
 		data_ptr->audioBlocks[i] = (float*) malloc(
 				sizeof(float) * vector_get(&vector, i));
-		printf("Block length[%d]: %d\n", i, vector_get(&vector, i));
+//		printf("Block length[%d]: %d\n", i, vector_get(&vector, i));
 	}
 
 	return data_ptr;
@@ -88,8 +103,7 @@ void printPartitionedImpulseFFTData(Vector vector, FFTData* fftData_ptr) {
 	}
 }
 
-void recombineBlocks(int length, Vector vector,
-		BlockData* data_ptr) {
+void recombineBlocks(int length, Vector vector, BlockData* data_ptr) {
 	//
 	//	writeWavFile(impulse, impulse->sampleRate, impulse->numChannels,
 	//			"output.wav");
@@ -124,6 +138,7 @@ FFTData* allocateFFTBuffers(BlockData* data_ptr, Vector vector) {
 	for (i = 0; i < fftData_ptr->size; i++) {
 		temp = calloc(vector_get(&vector, i), sizeof(complex));
 		for (j = 0; j < vector_get(&vector, i); j++) {
+//			printf("data_ptr->audioBlocks[%d][%d]: %f\n", i, j, data_ptr->audioBlocks[i][j]);
 			fftData_ptr->fftBlocks[i][j].Re = data_ptr->audioBlocks[i][j];
 		}
 		fft(fftData_ptr->fftBlocks[i], vector_get(&vector, i), temp);
@@ -143,11 +158,35 @@ static int paCallback(const void *inputBuffer, void *outputBuffer,
 	float *inBuf = (float*) inputBuffer;
 	float *outBuf = (float*) outputBuffer;
 
-	for (i = 0; i < framesPerBuffer; i++) {
+	// This loop is used to test interrupting the paCallback
+//	for (i=0;i<INT_MAX/2000; i++) {
+//	}
 
-		// Send each sample to the output
-		outBuf[i] = inBuf[i];
+	// FFT of block
+	{
+		complex *signal = calloc(framesPerBuffer, sizeof(complex));
+		complex *temp = calloc(framesPerBuffer, sizeof(complex));
+		for (i = 0; i < framesPerBuffer; i++) {
+			signal[i].Re = inBuf[i];
+		}
+
+		fft(signal, framesPerBuffer, temp);
+
+		ifft(signal, framesPerBuffer, temp);
+
+		for (i = 0; i < framesPerBuffer; i++) {
+			outBuf[i] = signal[i].Re / 50;
+		}
+
+		free(signal);
+		free(temp);
 	}
+
+//	for (i = 0; i < framesPerBuffer; i++) {
+//
+//		// Send each sample to the output
+//		outBuf[i] = inBuf[i];
+//	}
 
 	return paContinue;
 }
@@ -161,14 +200,14 @@ Vector determineBlockLengths(audioData* impulse) {
 	int blockSize = 2 * MIN_FFT_BLOCK_SIZE;
 	vector_append(&vector, blockSize);
 	remaining_length -= blockSize;
-	printf("block size: %d\n", blockSize);
+//	printf("block size: %d\n", blockSize);
 	//	printf("Remaining length: %d\n", length);
 	blockSize = MIN_FFT_BLOCK_SIZE;
 	// Subsequent blocks
 	while (remaining_length > 0) {
 		remaining_length -= blockSize;
 		vector_append(&vector, blockSize);
-		printf("block size: %d\n", blockSize);
+//		printf("block size: %d\n", blockSize);
 		//		printf("Remaining length: %d\n", length);
 		if (increment) {
 			blockSize *= 2;
@@ -180,35 +219,13 @@ Vector determineBlockLengths(audioData* impulse) {
 	return vector;
 }
 
-int main(int argc, char **argv) {
-
-	audioData *impulse = fileToBuffer("churchIR.wav");
-
-	audioData *signal = fileToBuffer("what.wav");
-
-	int length = calculateNextPowerOfTwo(
-			impulse->numChannels * impulse->numFrames);
-
-	impulse = zeroPadToNextPowerOfTwo(impulse);
-
-	Vector blockLengthVector = determineBlockLengths(impulse);
-
-	BlockData *data_ptr = allocateBlockBuffers(blockLengthVector);
-//
-	partitionImpulseIntoBlocks(blockLengthVector, data_ptr, impulse);
-//
-	FFTData* fftData_ptr = allocateFFTBuffers(data_ptr, blockLengthVector);
-
-	printPartitionedImpulseFFTData(blockLengthVector,fftData_ptr);
-//
-	PaStream *stream;
+void runPortAudio() {
+	PaStream* stream;
 	PaStreamParameters outputParameters;
 	PaStreamParameters inputParameters;
 	PaError err;
-
 	/* Initialize PortAudio */
 	Pa_Initialize();
-
 	/* Set output stream parameters */
 	outputParameters.device = Pa_GetDefaultOutputDevice();
 	outputParameters.channelCount = MONO;
@@ -216,7 +233,6 @@ int main(int argc, char **argv) {
 	outputParameters.suggestedLatency = Pa_GetDeviceInfo(
 			outputParameters.device)->defaultLowOutputLatency;
 	outputParameters.hostApiSpecificStreamInfo = NULL;
-
 	/* Set input stream parameters */
 	inputParameters.device = Pa_GetDefaultInputDevice();
 	inputParameters.channelCount = MONO;
@@ -224,21 +240,17 @@ int main(int argc, char **argv) {
 	inputParameters.suggestedLatency =
 			Pa_GetDeviceInfo(inputParameters.device)->defaultLowInputLatency;
 	inputParameters.hostApiSpecificStreamInfo = NULL;
-
 	/* Open audio stream */
 	err = Pa_OpenStream(&stream, &inputParameters, &outputParameters,
 	SAMPLE_RATE, MIN_FFT_BLOCK_SIZE, paNoFlag, paCallback, NULL);
-
 	if (err != paNoError) {
 		printf("PortAudio error: open stream: %s\n", Pa_GetErrorText(err));
 	}
-
 	/* Start audio stream */
 	err = Pa_StartStream(stream);
 	if (err != paNoError) {
 		printf("PortAudio error: start stream: %s\n", Pa_GetErrorText(err));
 	}
-
 	/* Get user input */
 	char ch = '0';
 	while (ch != 'q') {
@@ -246,7 +258,6 @@ int main(int argc, char **argv) {
 		ch = getchar();
 	}
 	err = Pa_StopStream(stream);
-
 	/* Stop audio stream */
 	if (err != paNoError) {
 		printf("PortAudio error: stop stream: %s\n", Pa_GetErrorText(err));
@@ -261,6 +272,43 @@ int main(int argc, char **argv) {
 	if (err != paNoError) {
 		printf("PortAudio error: terminate: %s\n", Pa_GetErrorText(err));
 	}
+}
+
+void loadImpulse() {
+	audioData* impulse = fileToBuffer("churchIR.wav");
+	audioData* signal = fileToBuffer("what.wav");
+	int length = calculateNextPowerOfTwo(
+			impulse->numChannels * impulse->numFrames);
+	impulse = zeroPadToNextPowerOfTwo(impulse);
+	Vector blockLengthVector = determineBlockLengths(impulse);
+	BlockData* data_ptr = allocateBlockBuffers(blockLengthVector);
+//	partitionImpulseIntoBlocks(blockLengthVector, data_ptr, impulse);
+	FFTData* fftData_ptr = allocateFFTBuffers(data_ptr, blockLengthVector);
+//	printPartitionedImpulseFFTData(blockLengthVector, fftData_ptr);
+}
+
+int main(int argc, char **argv) {
+
+	// structs for timing data
+	struct rusage before, after;
+
+	double time = 0;
+	getrusage(RUSAGE_SELF, &before);
+
+	int i;
+	for (i = 0; i < INT_MAX / 2000; i++) {
+	}
+
+	getrusage(RUSAGE_SELF, &after);
+
+	time += calculate(&before, &after);
+
+	printf("\nTime: %f\n", time);
+	printf("Time per block: %f\n", ((float) MIN_FFT_BLOCK_SIZE / SAMPLE_RATE));
+
+	loadImpulse();
+
+	runPortAudio();
 
 	return 0;
 }
